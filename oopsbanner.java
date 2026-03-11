9ce0c8dadd183ef77e7256d3d9ced8bcc810a343
Sai Abhijith Reddy <saiabhijithreddy@Abhijiths-Mac-377.local>
2026-03-11 10:50:21 +0530

    uc5 done

--- oopsbanner.java
+++ oopsbanner.java
@@ -1,14 +1,16 @@
 public class oopsbanner {
     public static void main(String[] args) {
-        //uc4
-        String[] lines = new String[7];
-        lines[0] = String.join(" ", " ***** ", " ***** ", " ****** ", " ***** ");
-        lines[1] = String.join(" ", "*     *", "*     *", "*     * ", "*     *");
-        lines[2] = String.join(" ", "*     *", "*     *", "******  ", "*      ");
-        lines[3] = String.join(" ", "*     *", "*     *", "*       ", " ***** ");
-        lines[4] = String.join(" ", "*     *", "*     *", "*       ", "      *");
-        lines[5] = String.join(" ", "*     *", "*     *", "*       ", "*     *");
-        lines[6] = String.join(" ", " ***** ", " ***** ", "*       ", " ***** ");
+        //uc5
+        String[] lines = {
+            String.join(" ", "  ***  ", "  ***  ", " ***** ", " ***** "),
+            String.join(" ", " *   * ", " *   * ", " *   * ", " *   * "),
+            String.join(" ", " *   * ", " *   * ", " *   * ", " *      "),
+            String.join(" ", " *   * ", " *   * ", " ***** ", " ***** "),
+            String.join(" ", " *   * ", " *   * ", " *      ", "      * "),
+            String.join(" ", " *   * ", " *   * ", " *      ", " *    * "),
+            String.join(" ", "  ***  ", "  ***  ", " *      ", "  ****  ")
+
+        };
         for (String line : lines) {
             System.out.println(line);
         }
