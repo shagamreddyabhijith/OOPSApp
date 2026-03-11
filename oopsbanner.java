6eb5a12ca3a1c53703a88bc3e27e46031de6d975
Sai Abhijith Reddy <saiabhijithreddy@Abhijiths-Mac-377.local>
2026-03-11 10:48:46 +0530

    uc3 done

--- oopsbanner.java
+++ oopsbanner.java
@@ -1,15 +1,10 @@
-public class oopsbanner{
-    //uc2
- public static void main(String args[]) {
-	System.out.println("   ***       ***    ******       *****");
-	System.out.println(" **   **   **   **  **    **   **");
-	System.out.println("**     ** **     ** **     ** **");
-	System.out.println("**     ** **     ** **    **   **");
-	System.out.println("**     ** **     ** ******       ***");
-	System.out.println("**     ** **     ** **              **");
-	System.out.println("**     ** **     ** **               **");
-	System.out.println(" **   **   **   **  **              **");
-	System.out.println("   ***       ***    **         *****");
-	}
+public class oopsbanner {
+    public static void main(String[] args) {
+        //uc3
+        System.out.println(String.join(" "," ***** "," ***** "," ******"," ***** "));
+        System.out.println(String.join(" ","*     *","*     *","*     *","*     *"));
+        System.out.println(String.join(" ","*     *","*     *","****** "," ***** "));
+        System.out.println(String.join(" ","*     *","*     *","*      ","      *"));
+        System.out.println(String.join(" "," ***** "," ***** ","*      "," ***** "));
+    }
 }
-
